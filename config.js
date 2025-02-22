dbcf8ec0be134af4e022ca27519412d12799f929
pancha2525 <alondra20346@gmail.com>
2025-02-22 11:27:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=EowhETRa#oKmgtI-6C8448cpmK8mQhfkATLh_0GQ0ylq8OzsksZ0' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'pancha2525' : process.env.GITHUB_USER_NAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94766087184' : process.env.OWNER_NUMBER
 };
